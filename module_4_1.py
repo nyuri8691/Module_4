9feb0846d0a3740558d717d14483f36e54c7279a
nyuri8691 <168839548+nyuri8691@users.noreply.github.com>
2024-12-20 15:02:45 +0300

    Add files via upload
    Домашняя работа по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+from fake_math import divide as fk_dv
+from true_math import divide as tr_dv
+print('result1 = ', fk_dv(69, 3))
+print('result2 = ', fk_dv(3, 0))
+print('result3 = ', tr_dv(49, 7))
+print('result4 = ', tr_dv(15, 0))
